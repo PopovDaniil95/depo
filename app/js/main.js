085d46a2bf1c888c22011b421d315f2cda170cf4
Daniil Popov <zikfaer12@gmail.com>
2023-09-01 07:27:04 +0700

    created hover efect

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,82 @@
-const faqItems = document.querySelectorAll(".faq__item");
+const accordionHeaders = document.querySelectorAll(".accordion__header");
 
-faqItems.forEach((item) => {
-  const title = item.querySelector(".faq__question");
-  const iconPlus = item.querySelector(".faq__icon");
-  const iconMinus = item.querySelector(".faq__icon.minus-icon");
-  const input = item.querySelector(".faq__input");
-  const faqText = item.querySelector(".faq__text");
+accordionHeaders.forEach((header) => {
+  const iconPlus = header.querySelector(".accordion__icon-plus");
+  const iconMinus = header.querySelector(".accordion__icon-minus");
 
-  title.addEventListener("click", () => {
-    if (input.checked) {
-      iconPlus.style.display = "inline-block";
-      iconMinus.style.display = "none";
-      faqText.classList.remove("expanded");
-    } else {
-      iconPlus.style.display = "none";
+  header.addEventListener("click", () => {
+    header.classList.toggle("active");
+    const content = header.nextElementSibling;
+    if (header.classList.contains("active")) {
+      content.style.maxHeight = content.scrollHeight + "px";
       iconMinus.style.display = "inline-block";
-      faqText.classList.add("expanded");
+      iconPlus.style.display = "none";
+    } else {
+      content.style.maxHeight = "0";
+      iconMinus.style.display = "none";
+      iconPlus.style.display = "inline-block";
     }
   });
+});
+
+const menuRussia = document.querySelector(".country__menu-russia");
+const russiaContent = document.querySelector(".country__map-russia");
+
+const menuUkraine = document.querySelector(".country__menu-ukraine");
+const ukraineContent = document.querySelector(".country__map-ukraine");
+
+const menuTurkey = document.querySelector(".country__menu-turkey");
+const turkeyContent = document.querySelector(".country__map-turkey");
+
+const menuKazakhstan = document.querySelector(".country__menu-kazakhstan");
+const kazakhstanContent = document.querySelector(".country__map-kazakhstan");
+
+const menuBelarus = document.querySelector(".country__menu-belarus");
+const belarusContent = document.querySelector(".country__map-belarus");
+
+menuRussia.addEventListener("mouseenter", () => {
+  russiaContent.style.opacity = "1";
+});
+
+menuRussia.addEventListener("mouseleave", () => {
+  russiaContent.style.opacity = "0.5";
+});
+
+menuUkraine.addEventListener("mouseenter", () => {
+  ukraineContent.style.opacity = "1";
+});
+
+menuUkraine.addEventListener("mouseleave", () => {
+  ukraineContent.style.opacity = "0.5";
+});
+
+menuBelarus.addEventListener("mouseenter", () => {
+  belarusContent.style.opacity = "1";
+});
+
+menuBelarus.addEventListener("mouseleave", () => {
+  belarusContent.style.opacity = "0.5";
+});
+
+menuTurkey.addEventListener("mouseenter", () => {
+  turkeyContent.style.opacity = "1";
+});
+
+menuTurkey.addEventListener("mouseleave", () => {
+  turkeyContent.style.opacity = "0.5";
+});
+
+menuKazakhstan.addEventListener("mouseenter", () => {
+  kazakhstanContent.style.opacity = "1";
+});
+
+menuKazakhstan.addEventListener("mouseleave", () => {
+  kazakhstanContent.style.opacity = "0.5";
+});
+
+const menuBtn = document.querySelector(".menu__btn-drop-down");
+const menuMobile = document.querySelector(".language-btn");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
 });
