dea3a7a7d363ec1966f8d0cf4fb923160c932e5f
Daniil Popov <zikfaer12@gmail.com>
2023-09-17 05:11:43 +0700

    make two mobal

--- app/js/fancybox.js
+++ app/js/fancybox.js
@@ -2,7 +2,4 @@ const btnModalConnect = document.querySelector(".btn_modal-connect");
 
 btnModalConnect.addEventListener("click", () => {
   Fancybox.close();
-  Fancybox.bind(document.getElementById("modal-connect-2"), "[data-fancybox]", {
-    // Your custom options
-  });
 });
